831b11732653afcd9e51a29073e77449583d0b15
Pavel Dedik <dedikx@gmail.com>
2013-07-30 18:59:09 +0200

    Added class to primary button in Announce popup.

--- applications/vanilla/views/discussion/announce.php
+++ applications/vanilla/views/discussion/announce.php
@@ -13,7 +13,7 @@ echo '<div class="P">', $this->Form->Radio('Announce', '@'.sprintf(T('In <b>%s</
 echo '<div class="P">', $this->Form->Radio('Announce', '@'.T("Don't announce."), array('Value' => '0')), '</div>';
 
 echo '<div class="Buttons Buttons-Confirm">';
-echo $this->Form->Button('OK');
+echo $this->Form->Button('OK', array('class' => 'Button Primary'));
 echo $this->Form->Button('Cancel', array('type' => 'button', 'class' => 'Button Close'));
 echo '<div>';
 echo $this->Form->Close();
