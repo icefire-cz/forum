30a207a5fd58f542f76ccc2e29f6223858c9d74a
Pavel Dedik <dedikx@gmail.com>
2013-07-31 12:28:47 +0200

    Fixed dropdown in profile.

--- applications/dashboard/views/modules/profileoptions.php
+++ applications/dashboard/views/modules/profileoptions.php
@@ -1,15 +1,26 @@
 <?php if (!defined('APPLICATION')) exit();
 /** Displays the "Edit My Profile" or "Back to Profile" buttons on the top of the profile page. */
-?>
-<div class="ProfileOptions">
-   <?php
+echo '<div class="ProfileOptions">';
    $Controller = Gdn::Controller();
    $Controller->FireEvent('BeforeProfileOptions');
-   echo ButtonGroup($Controller->EventArguments['MemberOptions'], 'NavButton MemberButtons');
-   echo ' ';
-   echo ButtonDropDown($Controller->EventArguments['ProfileOptions'], 
-      'NavButton ProfileButtons Button-EditProfile',
-      Sprite('SpEditProfile', 'Sprite16').' <span class="Hidden">'.T('Edit Profile').'</span>'
-   );
-   ?>
-</div>
+   $Links = $Controller->EventArguments['ProfileOptions'];
+   if (count($Links) == 0) {
+      echo ButtonGroup($Controller->EventArguments['MemberOptions'], 'btn');
+
+   } elseif (count($Links) == 1) {
+      $Link = array_pop($Links);
+      echo Anchor($Link['Text'], $Link['Url'], 'btn');
+
+   } else {
+      echo '<div class="btn-group">';
+      echo ButtonGroup($Controller->EventArguments['MemberOptions'], 'btn');
+      echo '<button class="btn dropdown-toggle" data-toggle="dropdown">';
+         echo '<span class="caret"></span>';
+      echo '</button>';
+      echo '<ul class="dropdown-menu">';
+         foreach ($Links as $Link) {
+            echo Wrap(Anchor($Link['Text'], $Link['Url'], GetValue('CssClass', $Link, '')), 'li');
+         }
+      echo '</ul></div>';
+   }
+echo'</div>';
