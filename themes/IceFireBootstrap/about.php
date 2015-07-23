c0def7cdfe4fb9db5a0bf86321161b4851d32828
Pavel Dedik <dedikx@gmail.com>
2015-07-23 19:45:00 +0200

    Created submodule from IceFireBootstrap theme.

--- themes/IceFireBootstrap/about.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-$ThemeInfo['IceFireBootstrap'] = array(
-   'Name' => 'IceFireBootstrap',
-   'Description' => 'Vanilla theme based on the Bootstrap framework by Twitter and VanillaBootstrap by Kasper Isager.',
-   'Version' => '2.1b-2.1',
-   'Author' => 'paulcz, Alek and Montezuma3',
-   'AuthorEmail' => 'dedikx@gmail.com',
-   'AuthorUrl' => 'http://www.icefire.cz'
-);
